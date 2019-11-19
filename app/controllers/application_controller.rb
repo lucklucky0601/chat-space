7ec545a04a7c180e88c0d1dddfcb1baf84be823d
lucklucky0601 <57529642+lucklucky0601@users.noreply.github.com>
2019-11-19 16:08:08 +0900

    chat-space

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  config.generators do |g|
+    g.stylesheets false
+    g.javascripts false
+    g.helper false
+    g.test_framework false
+  protect_from_forgery with: :exception
+end
