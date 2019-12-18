3b68a411afa355636db080be4e2f31ac94cac163
lucklucky0601 <57529642+lucklucky0601@users.noreply.github.com>
2019-12-19 02:33:58 +0900

    グループ機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,13 @@ class ApplicationController < ActionController::Base
     g.helper false
     g.test_framework false
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+end
 end
 end
